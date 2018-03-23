a287bb6acdc11b1d48f41e6def1e238dd15df032
arycoat <arycoat@naver.com>
2018-03-23 15:24:21 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
 @app.route('/keyboard', methods=['GET'])
 def keyboard():
     return 'OK'
+
+if __name__ == '__main__':
+    app.run(debug=True)
